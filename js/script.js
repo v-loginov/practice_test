0547197b20821e18ea192733beb15deb7511c466
v-loginov <v5120964@gmail.com>
2025-10-14 22:03:06 +0500

    Added processing name

--- js/script.js
+++ js/script.js
@@ -8,6 +8,12 @@ function isFirstOrStartMessage(cntMessages){
      else return false;
 }
 
+function isNameMessage(cntMessages){
+    if (cntMessages == 1)
+        {return true;}
+     else return false;
+}
+
 function counterMessage(i){
     countMessages = i;
 }
@@ -20,8 +26,15 @@ function newUserMessage(text){
         } else {
             console.log('Введите команду /start, для начала общения');
         }
+    } else if (isNameMessage(countMessages)){
+        if (text.trim().startsWith("/name:") & text.split(":")[1].trim().length > 0) {
+            console.log('Привет, ' + text.split(":")[1].trim() + ' приятно познакомиться. Я умею считать, введи числа которые надо посчитать');
+            counterMessage(2);
+        } else {
+            console.log('Для начала общения введите команду /name: Ваше имя (пустое имя не принимается)');
+        }
     } else {
-        console.log('Это не первое сообщение.');
+        console.log('Это третье сообщение');
     }
        
     }
