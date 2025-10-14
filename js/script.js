d65c9bee17515dde1b7987f325c1ac3f59dae786
v-loginov <v5120964@gmail.com>
2025-10-14 22:39:08 +0500

    Added processing numbers and operation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let countMessages = 0;
 let userInput = document.getElementById("userInput");
 let sendUserMessage = document.getElementById("sendUserMessage");
+let numberFirst = 0;
+let numberSecond = 0;
 
 function isFirstOrStartMessage(cntMessages){
     if (cntMessages == 0)
@@ -10,6 +12,18 @@ function isFirstOrStartMessage(cntMessages){
 
 function isNameMessage(cntMessages){
     if (cntMessages == 1)
+        {return true;}
+     else return false;
+}
+
+function isNumberMessage(cntMessages){
+    if (cntMessages == 2)
+        {return true;}
+     else return false;
+}
+
+function isOperationMessage(cntMessages){
+    if (cntMessages == 3)
         {return true;}
      else return false;
 }
@@ -27,15 +41,42 @@ function newUserMessage(text){
             console.log('Введите команду /start, для начала общения');
         }
     } else if (isNameMessage(countMessages)){
-        if (text.trim().startsWith("/name:") & text.split(":")[1].trim().length > 0) {
-            console.log('Привет, ' + text.split(":")[1].trim() + ' приятно познакомиться. Я умею считать, введи числа которые надо посчитать');
+        let userName = text.split(":")[1].trim();
+        if (text.trim().startsWith("/name:") & userName.length > 0) {
+            console.log('Привет, ' + userName + ' приятно познакомиться. Я умею считать, введи числа которые надо посчитать');
             counterMessage(2);
         } else {
-            console.log('Для начала общения введите команду /name: Ваше имя (пустое имя не принимается)');
+            console.log('Для продолжения общения введите команду /name: Ваше имя (пустое имя не принимается)');
         }
-    } else {
-        console.log('Это третье сообщение');
-    }
+    } else if (isNumberMessage(countMessages)){
+        let numbers = text.split(":")[1].trim();
+        let numbersArray = numbers.split(',');
+        if (text.trim().startsWith("/number:") & numbersArray.length == 2){
+            numberFirst = parseFloat(numbersArray[0]);
+            numberSecond = parseFloat(numbersArray[1]);
+            console.log('Для продолжения вычисления введите одно из действий: -, +, *, /');
+            counterMessage(3);
+        }else {
+            console.log('Для продолжения общения введите команду /number: ПервоеЧисло, ВтороеЧисло (должно быть два числа разделённых запятой)');
+        }
+    } else if (isOperationMessage(countMessages)){
+        switch(text) {
+            case '+':
+                console.log('Результат: ' + numberFirst + numberSecond);
+                break;
+            case '-':
+                console.log('Результат: ' + numberFirst - numberSecond);
+                break;
+            case '*':
+                console.log('Результат: ' + numberFirst * numberSecond);
+                break;
+            case '/':
+                console.log('Результат: ' + numberFirst / numberSecond);
+                break;
+            default:
+                console.log('Для продолжения вычисления введите одно из действий: -, +, *, /');
+        }
+    } 
        
     }
     
