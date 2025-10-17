e330fbae5bac357d3381374bf5ab192db262ad03
v-loginov <v5120964@gmail.com>
2025-10-17 22:32:01 +0500

    Refactoring

--- js/script.js
+++ js/script.js
@@ -41,30 +41,24 @@ function newUserMessage(text){
     if (isFirstOrStartMessage(countMessages)) {
         if (text.trim() == '/start') {
             viewBotMessage('Привет, меня зовут Чат-бот, а как зовут тебя? (Для справки: В любой момент ты можешь завершить общение командой /stop)');
-            console.log('Привет, меня зовут Чат-бот, а как зовут тебя? (Для справки: В любой момент ты можешь завершить общение командой /stop)');
             counterMessage(1);
         } else {
             viewBotMessage('Введите команду /start, для начала общения');
-            console.log('Введите команду /start, для начала общения');
         }
     } else if (isNameMessage(countMessages)){
         if (text.trim().startsWith("/name:")) {
             let userName = text.split(':')[1];
             if (userName.trim().length > 0) {
                 viewBotMessage('Привет, ' + userName.trim() + ' приятно познакомиться. Я умею считать, введи числа которые надо посчитать.');
-                console.log('Привет, ' + userName.trim() + ' приятно познакомиться. Я умею считать, введи числа которые надо посчитать.');
                 counterMessage(2);
             } else {
                 viewBotMessage('Для продолжения общения введите команду /name: Ваше имя (пустое имя не принимается)');
-                console.log('Для продолжения общения введите команду /name: Ваше имя (пустое имя не принимается)');
             }
         } else if (text.trim() == '/stop'){
             viewBotMessage('Всего доброго, если хочешь поговорить пиши /start');
-            console.log('Всего доброго, если хочешь поговорить пиши /start');
             counterMessage(0);
         } else {
             viewBotMessage('Для продолжения общения введите команду /name: Ваше имя (пустое имя не принимается)');
-            console.log('Для продолжения общения введите команду /name: Ваше имя (пустое имя не принимается)');
         }
     } else if (isNumberMessage(countMessages)){
                 if (text.trim().startsWith("/number:")){
@@ -74,19 +68,15 @@ function newUserMessage(text){
                         numberFirst = parseFloat(numbersArray[0].trim());
                         numberSecond = parseFloat(numbersArray[1].trim());
                         viewBotMessage('Для вычисления введите одно из действий: -, +, *, /');
-                        console.log('Для вычисления введите одно из действий: -, +, *, /');
                         counterMessage(3);
                     } else {
                         viewBotMessage('Для продолжения общения введите команду /number: ПервоеЧисло, ВтороеЧисло (должно быть два числа разделённых запятой)');
-                        console.log('Для продолжения общения введите команду /number: ПервоеЧисло, ВтороеЧисло (должно быть два числа разделённых запятой)');
                     }
                 } else if (text.trim() == '/stop'){
                     viewBotMessage('Всего доброго, если хочешь поговорить пиши /start');
-                    console.log('Всего доброго, если хочешь поговорить пиши /start');
                     counterMessage(0);
                 } else {
                     viewBotMessage('Для продолжения общения введите команду /number: ПервоеЧисло, ВтороеЧисло (должно быть два числа разделённых запятой)');
-                    console.log('Для продолжения общения введите команду /number: ПервоеЧисло, ВтороеЧисло (должно быть два числа разделённых запятой)');
                 }
     } else if (isOperationMessage(countMessages)){
         if (text.trim().length = 1) {
@@ -95,42 +85,31 @@ function newUserMessage(text){
             case '+':
                 result = numberFirst + numberSecond;
                 viewBotMessage('Результат: ' + result);
-                console.log('Результат: ' + result);
                 counterMessage(2);
-                console.log('Для продолжения общения введите команду /number: ПервоеЧисло, ВтороеЧисло (должно быть два числа разделённых запятой)');
                 break;
             case '-':
                 result = numberFirst - numberSecond;
                 viewBotMessage('Результат: ' + result);
-                console.log('Результат: ' + result);
                 counterMessage(2);
-                console.log('Для продолжения общения введите команду /number: ПервоеЧисло, ВтороеЧисло (должно быть два числа разделённых запятой)');
                 break;
             case '*':
                 result = numberFirst * numberSecond;
                 viewBotMessage('Результат: ' + result);
-                console.log('Результат: ' + result);
                 counterMessage(2);
-                console.log('Для продолжения общения введите команду /number: ПервоеЧисло, ВтороеЧисло (должно быть два числа разделённых запятой)');
                 break;
             case '/':
                 result = numberFirst / numberSecond;
                 viewBotMessage('Результат: ' + result);
-                console.log('Результат: ' + result);
                 counterMessage(2);
-                console.log('Для продолжения общения введите команду /number: ПервоеЧисло, ВтороеЧисло (должно быть два числа разделённых запятой)');
                 break;
             default:
                 viewBotMessage('Для вычисления введите одно из действий: -, +, *, /');
-                console.log('Для вычисления введите одно из действий: -, +, *, /');
             }
         } else if (text.trim() == '/stop'){
             viewBotMessage('Всего доброго, если хочешь поговорить пиши /start');
-            console.log('Всего доброго, если хочешь поговорить пиши /start');
             counterMessage(0);
         } else {
             viewBotMessage('Для вычисления введите одно из действий: -, +, *, /');
-            console.log('Для вычисления введите одно из действий: -, +, *, /');
         }
         
     } 
